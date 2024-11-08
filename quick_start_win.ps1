8b2fc04322512624e406ccfa34563cd32a648cf8
Aidar Kutdusov <aydar2k@mail.ru>
2024-11-08 16:43:13 +0300

    added quick start files for multiple os

--- quick_start_win.ps1
+++ quick_start_win.ps1
@@ -1,4 +1,4 @@
-# 0. CREATE VIRTUAL ENVIRONMENT IF IT DOES NOT EXIST
+# 1. CREATE VIRTUAL ENVIRONMENT IF IT DOES NOT EXIST
 Write-Output "CREATING VIRTUAL ENVIRONMENT VENV..."
 if (!(Test-Path -Path "./venv")) {
     python -m venv venv
@@ -7,33 +7,33 @@ if (!(Test-Path -Path "./venv")) {
     Write-Output "VIRTUAL ENVIRONMENT ALREADY EXISTS."
 }
 
-# 1. ACTIVATE VIRTUAL ENVIRONMENT
+# 2. ACTIVATE VIRTUAL ENVIRONMENT
 Write-Output "ACTIVATING VIRTUAL ENVIRONMENT..."
 # Activation for current script
 & .\venv\Scripts\Activate
 
-# 2. INSTALL REQUIREMENTS FROM requirements.txt
+# 3. INSTALL REQUIREMENTS FROM requirements.txt
 Write-Output "INSTALLING DEPENDENCIES FROM requirements.txt..."
 pip install -r requirements.txt
 
-# 3. INSTALL ADDITIONAL DEPENDENCIES
+# 4. INSTALL ADDITIONAL DEPENDENCIES
 Write-Output "INSTALLING ADDITIONAL DEPENDENCIES..."
 pip install git+https://github.com/grigandal625/AT_QUEUE.git@master
 pip install git+https://github.com/grigandal625/AT_SOLVER.git@master
 pip install git+https://github.com/grigandal625/AT_TEMPORAL_SOLVER.git@master
 pip install git+https://github.com/grigandal625/AT_BLACKBOARD.git@master
 
-# 4. CREATE CONTAINER WITH RABBITMQ
+# 5. CREATE CONTAINER WITH RABBITMQ
 Write-Output "CREATING CONTAINER WITH RABBITMQ..."
 docker run --name rabbit_at_parking -p 15672:15672 -p 5672:5672 -d rabbitmq:management
 
-# 5. LAUNCH COMPONENTS IN NEW POWERSHELL WINDOWS AND KEEP THEM OPEN
-Write-Output "LAUNCHING COMPONENTS IN NEW POWERSHELL WINDOWS..."
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_queue; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_temporal_solver; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_solver; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_blackboard; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python logic/planner/ATAgentPlanner.py; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
+# # 6. LAUNCH COMPONENTS IN NEW POWERSHELL WINDOWS AND KEEP THEM OPEN
+# Write-Output "LAUNCHING COMPONENTS IN NEW POWERSHELL WINDOWS..."
+#
+# Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_queue; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
+# Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_temporal_solver; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
+# Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_solver; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
+# Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python -m at_blackboard; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
+# Start-Process powershell -ArgumentList "-NoExit", "-Command", "venv\Scripts\activate; python logic/planner/ATAgentPlanner.py; Read-Host 'PRESS ENTER TO CLOSE WINDOW'"
 
 Write-Output "ALL PROCESSES STARTED."
